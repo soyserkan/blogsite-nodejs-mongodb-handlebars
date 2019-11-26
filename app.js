88648a429ec8470813de2a02338fbba5d36870d9
soyserkan <serkansoy94@gmail.com>
2019-11-27 00:00:12 +0300

    26.11.2019 Genel Güncelleme

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
+const mongoose = require('mongoose');
 
 const app = express();
 
+(async () => {
+    try {
+        await mongoose.connect('mongodb+srv://serkan:qRJ2gXt8mzb84oEE@cluster0-axzgd.mongodb.net/blog-site?retryWrites=true&w=majority', { useUnifiedTopology: true, useNewUrlParser: true });
+        console.log("MongoDB Connected");
+    } catch (err) {
+        console.log("MongoDB Connection Error: ", err);
+    }
+})();
+
+
+
 
 app.use(express.static(`${__dirname}/public`));
-
 
 app.engine('handlebars', exphbs({
     defaultLayout: 'home'
@@ -15,9 +26,11 @@ app.set('view engine', 'handlebars');
 
 const mainRoutes = require('./routes/home/index');
 const adminRoutes = require('./routes/admin/index');
+const postsRoutes = require('./routes/admin/posts');
 
-app.use('/',mainRoutes);
-app.use('/admin',adminRoutes);
+app.use('/', mainRoutes);
+app.use('/admin', adminRoutes);
+app.use('/admin/posts', postsRoutes);
 
 
 
