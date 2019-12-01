49e5b85166a6307c3e8036a642eef4b2b621aee8
soyserkan <serkansoy94@gmail.com>
2019-12-01 22:37:13 +0300

    registeration is completed

--- app.js
+++ app.js
@@ -54,6 +54,7 @@ app.use(flash());
 
 app.use((req, res, next) => {
     res.locals.success_message = req.flash('success_message');
+    res.locals.error_message = req.flash('error_message');
     next();
 })
 
--- routes/home/index.js
+++ routes/home/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const Post = require('../../models/Post');
 const Category = require('../../models/Category');
+const User = require('../../models/User');
+const bcrypt = require('bcryptjs');
 
 
 router.all('/*', (req, res, next) => {
@@ -12,9 +14,9 @@ router.all('/*', (req, res, next) => {
 router.get('/', async (req, res) => {
 
     try {
-        const postData = await Post.find({});
+        const postData = await Post.find({}).populate('category');
         const categoryData = await Category.find({});
-        res.render('home/index', { postData,categoryData });
+        res.render('home/index', { postData, categoryData });
     } catch (error) {
         console.log(`Error occured: ${error}`);
     }
@@ -23,17 +25,60 @@ router.get('/', async (req, res) => {
 router.get('/about-us', (req, res) => {
     res.render('home/about-us');
 });
+
 router.get('/login', (req, res) => {
-    res.render('home/login');
+    res.render('home/login', { layout: false });
+});
+
+router.get('/register', (req, res) => {
+    res.render('home/register', { layout: false });
+});
+
+router.post('/register', async (req, res) => {
+
+    let errors = [];
+
+    if (req.body.password != req.body.confirmPassword) {
+        errors.push({ message: 'passwords are not mached!' });
+    }
+    if (errors.length > 0) {
+        res.render('home/register', {
+            layout: false,
+            errors,
+            name: req.body.name,
+            surname: req.body.surname,
+            email: req.body.email
+        });
+
+    } else {
+        const user = await User.findOne({ email: req.body.email });
+        if (!user) {
+            const newUser = await new User({
+                name: req.body.name,
+                surname: req.body.surname,
+                email: req.body.email,
+                password: req.body.password
+            });
+            var salt = bcrypt.genSaltSync(10);
+            var hash = bcrypt.hashSync(newUser.password, salt);
+            newUser.password = hash;
+            await newUser.save();
+            await req.flash('success_message', 'You are now registered, please login');
+            res.redirect('/login');
+        } else {
+            req.flash('error_message', 'That mail exist, please login');
+            res.redirect('/login');
+        }
+    }
 });
 
 router.get('/post-detail/:id', async (req, res) => {
     try {
         const postData = await Post.findOne({
             _id: req.params.id
-        });
+        }).populate('category');
         const categoryData = await Category.find({});
-        res.render('home/post-detail', { postData,categoryData });
+        res.render('home/post-detail', { postData, categoryData });
     } catch (error) {
         console.log(`Error occured: ${error}`);
     }
